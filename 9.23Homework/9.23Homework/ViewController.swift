fa1ac4ece0bafd26dcd2e5891c16f2f001ad2ebe
Yao Xiao <xiao.yao7@northeastern.edu>
2023-10-02 17:54:34 -0700

    Committing 9.23 Homework

---
+++ 9.23Homework/9.23Homework/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  9.23Homework
+//
+//  Created by Yao on 2023/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    let imageNames = ["Hamburger", "Pizza", "Dumpling", "Pasta"]
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = Bundle.main.loadNibNamed("ImageTableViewCell", owner: self)?.first as! ImageTableViewCell
+        
+        cell.imgView?.image = UIImage(named: imageNames[indexPath.row])
+        cell.lblImage.text = imageNames[indexPath.row]
+        
+        return cell
+    }
+
+}
+
